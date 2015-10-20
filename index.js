ebeb6a890c50092076ab20841312c78e29c1aae0
sanemat <o.gata.ken@gmail.com>
2015-10-20 16:52:37 +0900

    style(indent): indent for comment by eclint

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 /**
- * @return {string} plain-text title and url from current page.
- */
+  * @return {string} plain-text title and url from current page.
+  **/
 module.exports = function () {
   return window.document.title + ' ' + window.location.href;
 };

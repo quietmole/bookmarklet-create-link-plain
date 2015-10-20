edd27349634d05751813673e674de81d4c94c80a
sanemat <o.gata.ken@gmail.com>
2015-10-20 22:46:42 +0900

    chore(test): use custom page

--- test.js
+++ test.js
@@ -2,6 +2,6 @@
 var assert = require('power-assert');
 var browserTitleOrSelection = require('./');
 
-it('should be title and url', function () {
-  assert(browserTitleOrSelection() === 'Test\'em');
+it('should be title', function () {
+  assert(browserTitleOrSelection() === 'Current page title');
 });
